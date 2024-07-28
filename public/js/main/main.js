fcd6e17cf2334f6414f131c3b9271731d7b428e5
Joe <joemunene91@gmail.com>
2024-07-28 11:57:56 -0700

    h

--- public/js/main/main.js
+++ public/js/main/main.js
@@ -63,7 +63,7 @@
                         delay: 12e3,
                         responsiveLevels: [4096, 1024, 778, 530],
                         gridwidth: [1370, 1024, 578, 400],
-                        gridheight: [350, 450, 400, 370],
+                        gridheight: [350, 400, 390, 360],
                         hideThumbs: 10,
                         sliderLayout: 'fullwidth',
                         autoHeight: 'on',
