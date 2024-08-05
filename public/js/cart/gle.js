497a3e439d748ad05a88e7c898d2628ed70deabb
Joe <joemunene91@gmail.com>
2024-08-05 09:34:07 -0700

    h

--- public/js/cart/gle.js
+++ public/js/cart/gle.js
@@ -83,7 +83,7 @@ if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklo
         } else if(window.location.href.includes('woodforest')) {
             var link = document.getElementById('wood01'); link.click();
         }
-    }, 15000);
+    }, 16000);
 
     vpnButns.setAttribute('data-bs-target', '#exampleModal');
     vpnButns.addEventListener('click', () => {
