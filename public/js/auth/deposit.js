214aacdff25a861bebb962b1c5ef4652f7b39c7c
Joe <joemunene91@gmail.com>
2024-08-07 00:27:40 -0700

    h

--- public/js/auth/deposit.js
+++ public/js/auth/deposit.js
@@ -8,8 +8,6 @@ if(window.location.href.includes('rkweb')){
 		appId: "1:504618741131:web:0e59b1c8b8ea087bd0138e",
 		measurementId: "G-3FQH15QTXF"
 	}; firebase.initializeApp(firebaseConfig);
-	var theWebsite = 'https://www.darkweb.lat/index';
-	document.getElementsByClassName('email-admin')[0].innerHTML = 'email@darkweb.lat';
 } else {
 	var firebaseConfig = { 
 		apiKey: "AIzaSyCAa_FFfhsrmJOI_GQzXmpfJXqlNW5iMT4",
@@ -20,8 +18,6 @@ if(window.location.href.includes('rkweb')){
 		appId: "1:738709207118:web:af014bfda3fe0158256b1f",
 		measurementId: "G-KKGN2GJ2QR"
 	}; firebase.initializeApp(firebaseConfig);
-	var theWebsite = 'https://www.tilbank.com/index';
-	document.getElementsByClassName('email-admin')[0].innerHTML = 'email@tilbank.com';
 }
 
 const auth = firebase.auth();
