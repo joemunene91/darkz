8849791d5dfbd89636379acc373b08bd4b0f4129
Joe <joemunene91@gmail.com>
2024-08-24 12:24:28 -0700

    h

--- public/js/auth/home.js
+++ public/js/auth/home.js
@@ -50,12 +50,8 @@ auth.onAuthStateChanged(user => {
 		} 
 	
 		if(user.email) {
-			var theaddress = (user.email).substring(0, (user.email).indexOf('@'));
-			if (user.displayName) { theaddress = user.displayName } 
-			jinaHolder.value = theaddress;
 			jinaHolder2.innerHTML = user.email;
 		} else if(user.phoneNumber) {
-			jinaHolder.value = 'Home Page'
 			jinaHolder2.innerHTML = 'Phone: ' + user.phoneNumber;
 		} 
 
